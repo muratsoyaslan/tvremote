ec9fce803299cb683be195fd82ee61f78453d015
muratsoyaslan <m.soyaslan1907@gmail.com>
2020-12-06 17:04:39 +0300

    testmurat

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 a= int(input("Birinci Sayı:"))
 b= int(input("İkinci Sayı:"))
 
-işlem= (input("işlemi giriniz:"))
+islem= (input("işlemi giriniz:"))
 
-if işlem == "1" :
+if islem == "1inci" :
    print("{} ile  {} in toplamı {} dır".format(a,b,a+b))
-elif işlem == "2":  
+elif islem == "2nci":  
    print("{} ile  {} in farkı {} dır".format(a,b,a-b))
-elif işlem == "3":   
+elif islem == "3üncü":   
    print("{} ile  {} in çarpımı {} dır".format(a,b,a*b)) 
-elif işlem == "4":   
+elif islem == "4üncü":   
    print("{} ile  {} in bölümü {} dır".format(a,b,a/b)) 
 else:
    print("sonuç bulunamadı")    
